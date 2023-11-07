404144cc6b1b5cbab2e9d48fa7507d8fc5adf833
qlhan <qlhan@incarcloud.com>
2023-11-07 15:31:49 +0800

    feat: task query + complete
    step 04

--- at7-flow-backend/src/test/java/com/at7/flow/BpmnTests.java
+++ at7-flow-backend/src/test/java/com/at7/flow/BpmnTests.java
@@ -5,12 +5,15 @@ import org.activiti.engine.*;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 /**
  * BpmnTests
@@ -45,7 +48,7 @@ public class BpmnTests {
 
     @Disabled
     @Test
-    public void testDeploy() throws Exception {
+    public void testFlowDeploy() throws Exception {
         // ACT_GE_PROPERTY    更新版本号
         // ACT_RE_DEPLOYMENT  流程信息
         // ACT_RE_PROCDEF     流程定义
@@ -63,8 +66,9 @@ public class BpmnTests {
         log.info("id = {}, name = {}", deployment.getId(), deployment.getName());
     }
 
+    @Disabled
     @Test
-    public void testStart() throws Exception {
+    public void testFlowStart() throws Exception {
         // ACT_GE_PROPERTY    更新版本号
         // ACT_HI_ACTINST
         // ACT_HI_IDENTITYLINK
@@ -76,7 +80,46 @@ public class BpmnTests {
         ProcessEngine processEngine = standaloneProcessEngineConfiguration.buildProcessEngine();
         RuntimeService runtimeService = processEngine.getRuntimeService();
         ProcessInstance instance = runtimeService.startProcessInstanceByKey("Test01");
-        // aId=null, dId=Test01:1:2504, id=5001
-        log.info("aId={}, dId={}, id={}", instance.getActivityId(), instance.getProcessDefinitionId(), instance.getId());
+        // pdId=Test01:1:2504, id=5001
+        log.info("pdId={}, id={}", instance.getProcessDefinitionId(), instance.getId());
+    }
+
+    @Disabled
+    @Test
+    public void testTaskQuery() throws Exception {
+        ProcessEngine processEngine = standaloneProcessEngineConfiguration.buildProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> taskList = taskService.createTaskQuery()
+                .processDefinitionKey("Test01")
+                .taskAssignee("aa")
+                .list();
+        for (Task task : taskList) {
+            log.info("pdId={}, id={}, name={}", task.getProcessDefinitionId(), task.getId(), task.getName(), task.getName());
+        }
+    }
+
+    @Test
+    public void testTaskComplete() throws Exception {
+        // ACT_GE_PROPERTY
+        // ACT_RU_TASK
+        // ACT_RU_VARIABLE
+        // ACT_RE_PROCDEF
+        // ACT_RE_DEPLOYMENT
+        // ACT_GE_BYTEARRAY
+        // ACT_RE_PROCDEF
+        // ACT_PROCDEF_INFO
+
+        // ACT_HI_TASKINST
+        // ACT_HI_ACTINST
+        // ACT_HI_IDENTITYLINK
+        // ACT_RU_TASK
+        // ACT_RU_IDENTITYLINK
+        ProcessEngine processEngine = standaloneProcessEngineConfiguration.buildProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+        Task task = taskService.createTaskQuery()
+                .processDefinitionKey("Test01")
+                .taskAssignee("cc")
+                .singleResult();
+        taskService.complete(task.getId());
     }
 }
