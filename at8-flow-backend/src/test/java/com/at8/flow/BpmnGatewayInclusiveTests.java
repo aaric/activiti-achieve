4df731996d931d6e495af4ad2726c5258deade8a
qlhan <qlhan@incarcloud.com>
2023-11-10 17:04:13 +0800

    test: gateway inclusive
    Process08

--- at8-flow-backend/src/test/java/com/at8/flow/BpmnGatewayInclusiveTests.java
+++ at8-flow-backend/src/test/java/com/at8/flow/BpmnGatewayInclusiveTests.java
@@ -15,11 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashMap;
-import java.util.zip.ZipInputStream;
 
 /**
  * BpmnGatewayInclusiveTests
@@ -37,7 +35,7 @@ public class BpmnGatewayInclusiveTests {
 
     private String bizKey = "custom011";
 
-//    @Disabled
+    @Disabled
     @Test
     public void testFlowDeployZip() throws Exception {
         ProcessEngine processEngine = standaloneProcessEngineConfiguration.buildProcessEngine();
@@ -45,16 +43,15 @@ public class BpmnGatewayInclusiveTests {
 
 //        repositoryService.deleteDeployment("5001", true);
 
-        ClassPathResource classPathResource = new ClassPathResource("processes/Process08.zip");
-        ZipInputStream zipInputStream = new ZipInputStream(classPathResource.getInputStream());
         Deployment deployment = repositoryService.createDeployment()
-                .addZipInputStream(zipInputStream)
+                .addClasspathResource("processes/Process08.bpmn20.xml")
+                .addClasspathResource("processes/Process08.png")
                 .name("测试流程08")
                 .deploy();
         log.info("id = {}, name = {}", deployment.getId(), deployment.getName());
     }
 
-//    @Disabled
+    @Disabled
     @Test
     public void testFlowStart() throws Exception {
         ProcessEngine processEngine = standaloneProcessEngineConfiguration.buildProcessEngine();
@@ -70,14 +67,14 @@ public class BpmnGatewayInclusiveTests {
         log.info("pdId={}, id={}", instance.getProcessDefinitionId(), instance.getId());
     }
 
-//    @Disabled
+    @Disabled
     @Test
     public void testTaskComplete() throws Exception {
         ProcessEngine processEngine = standaloneProcessEngineConfiguration.buildProcessEngine();
         TaskService taskService = processEngine.getTaskService();
         Task task = taskService.createTaskQuery()
                 .processDefinitionKey("Process08")
-                .taskAssignee("dd")
+                .taskAssignee("ee")
                 .singleResult();
 
         if (null != task) {
